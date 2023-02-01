2d14dd70e73d0e62593b0634f5fcf93a3ebd25b9
doch13_ <dohui0922@dimigo.hs.kr>
2023-02-01 22:03:54 +0900

    feat: Register Main Page

---
+++ lib/pages/register/widgets/register_button.dart
@@ -0,0 +1,81 @@
+import 'package:dalgeurak_meal_application/routes/routes.dart';
+import 'package:dimigoin/themes/text_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+
+import '../../../themes/color_theme.dart';
+
+enum RegisterButtonMode {
+  laundry,
+  reveille,
+  weekendStay,
+  mealException,
+  specialRoom,
+  fridayHomecoming
+
+}
+
+extension RegisterButtonModeExtension on RegisterButtonMode {
+  String get getIconName {
+    switch (this) {
+      case RegisterButtonMode.laundry: return "washer";
+      case RegisterButtonMode.reveille: return "music";
+      case RegisterButtonMode.weekendStay: return "book";
+      case RegisterButtonMode.mealException: return "tableware";
+      case RegisterButtonMode.specialRoom: return "specialRoom";
+      case RegisterButtonMode.fridayHomecoming: return "homecoming";
+      default: return "";
+    }
+  }
+
+  String get getTitle {
+    switch (this) {
+      case RegisterButtonMode.laundry: return "세탁";
+      case RegisterButtonMode.reveille: return "기상송";
+      case RegisterButtonMode.weekendStay: return "주말잔류";
+      case RegisterButtonMode.mealException: return "선밥/후밥";
+      case RegisterButtonMode.specialRoom: return "특별실";
+      case RegisterButtonMode.fridayHomecoming: return "금요귀가";
+      default: return "";
+    }
+  }
+
+  dynamic get getTapFunction {
+    switch (this) {
+      case RegisterButtonMode.mealException: return () => Get.toNamed(DalgeurakMealApplicationRoutes.MEALEXCEPTION);
+      default: return () => print("onClick");
+    }
+  }
+}
+
+class RegisterButton extends GetWidget {
+  RegisterButtonMode btnMode;
+
+  RegisterButton({required this.btnMode});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: btnMode.getTapFunction,
+      child: Container(
+        height: 125,
+        width: 165,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: DimigoinColor.C6,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset("assets/images/register/${btnMode.getIconName}.svg", width: 60),
+            const SizedBox(height: 8),
+            Text("${btnMode.getTitle}", style: DimigoinTextStyle.T4)
+          ],
+        ),
+      ),
+    );
+  }
+
+}
--- lib/themes/text_theme.dart
+++ lib/themes/text_theme.dart
@@ -2,5 +2,15 @@ import 'package:flutter/material.dart';
 
 abstract class DimigoinTextStyle {
   static const T1 = TextStyle(
-      fontSize: 14, color: Colors.black, fontFamily: 'Apple', fontWeight: FontWeight.w500);
+      fontSize: 24, color: Colors.black, fontFamily: 'AppleSDGothicNeo', fontWeight: FontWeight.w700);
+  static const T2 = TextStyle(
+      fontSize: 22, color: Colors.black, fontFamily: 'AppleSDGothicNeo', fontWeight: FontWeight.w700);
+  static const T3 = TextStyle(
+      fontSize: 18, color: Colors.black, fontFamily: 'AppleSDGothicNeo', fontWeight: FontWeight.w700);
+  static const T4 = TextStyle(
+      fontSize: 16, color: Colors.black, fontFamily: 'AppleSDGothicNeo', fontWeight: FontWeight.w700);
+  static const T5 = TextStyle(
+      fontSize: 14, color: Colors.black, fontFamily: 'AppleSDGothicNeo', fontWeight: FontWeight.w700);
+  static const T6 = TextStyle(
+      fontSize: 12, color: Colors.black, fontFamily: 'AppleSDGothicNeo', fontWeight: FontWeight.w700);
 }

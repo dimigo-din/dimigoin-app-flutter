31364495dc74f8f016efa9a1a52571893b6e33e7
doch13_ <dohui0922@dimigo.hs.kr>
2023-02-01 20:57:31 +0900

    refactor: TextStyle & Color Theme Appoint

---
+++ lib/themes/text_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class DimigoinTextStyle {
+  static const T1 = TextStyle(
+      fontSize: 14, color: Colors.black, fontFamily: 'Apple', fontWeight: FontWeight.w500);
+}
